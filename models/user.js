bf2a890183fff41db4c6aafa7dac4c54a2463016
Subham Mishra <imsubham882@gmail.com>
2018-10-01 13:48:57 +0530

    Added Project Files

---
+++ models/user.js
@@ -0,0 +1,58 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+mongoose.connect("mongodb://localhost:27017/nodeauth" , { useNewUrlParser: true }).then(
+  (res) => {
+   console.log("Connected to Database Successfully.")
+  }
+).catch(() => {
+  console.log("Conntection to database failed.");
+});
+
+var db = mongoose.connection;
+
+// User Schema
+var UserSchema = mongoose.Schema({
+	username: {
+		type: String,
+		index: true
+	},
+	password: {
+		type: String
+	},
+	email: {
+		type: String
+	},
+	name: {
+		type: String
+	},
+	profileimage:{
+		type: String
+	}
+});
+
+var User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = function(id, callback){
+	User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function(username, callback){
+	var query = {username: username};
+	User.findOne(query, callback);
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+    	callback(null, isMatch);
+	});
+}
+
+module.exports.createUser = function(newUser, callback){
+	bcrypt.genSalt(10, function(err, salt) {
+    	bcrypt.hash(newUser.password, salt, function(err, hash) {
+   			newUser.password = hash;
+   			newUser.save(callback);
+    	});
+	});
+}
